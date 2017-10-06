23777a7cf1b10556de47b84d17bbfa8fe3ce8777
Daniel Stokes <danielphilipstokes@gmail.com>
2017-10-06 09:40:12 -0400

    WebGL Build

---
+++ Unity/Assets/Bomb.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+    public LayerMask ExplodeLayerMask;
+
+	// Use this for initialization
+	void Awake () {
+        StartCoroutine(Explode());
+    }
+	
+	// Update is called once per frame
+
+
+    IEnumerator Explode() {
+        yield return new WaitForSeconds(2);
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, 5 , Vector2.zero, 50, ExplodeLayerMask);
+        foreach (RaycastHit2D hit in hits) {
+
+            Vector3 direction = hit.transform.position - transform.position;
+            float distance = Vector3.Distance(transform.position, hit.transform.position) * 50;
+
+
+
+            Vector3 force = direction * (250 - distance);
+
+            hit.transform.GetComponent<Rigidbody2D>().AddForce(force);
+            Destroy(gameObject);
+
+        }
+
+    }
+}
---
+++ Unity/Assets/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    public GameObject bombPrefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            spawnPosition.z = 0;
+
+            Instantiate(bombPrefab, spawnPosition, Quaternion.identity);
+        }
+	}
+}
